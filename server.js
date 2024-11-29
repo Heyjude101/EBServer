f2670831731a4ae28380b77c21406cf1fe254d4d
heyjude101 <ritikraj2884@gmail.com>
2024-11-29 15:24:11 +0530

    second commit test

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/health', (req, res) => {
   res.status(200).send('Working');
 });
 
+app.get('/signup', (req, res) => {
+  res.status(200).send('Signup Working');
+});
+
+app.get('/login', (req, res) => {
+  res.status(200).send('Login Working');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
 });
